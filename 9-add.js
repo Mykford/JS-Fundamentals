e02b2b9f537de00e5f2ffef347c512557e01b8a7
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-19 22:14:23 +0000

    modify add function

--- 9-add.js
+++ 9-add.js
@@ -1,4 +1,8 @@
 function sum(a, b) {
-  let result = a + b;
-  console.log(result);
+  if (isNaN(a) || isNaN(b)) {
+    console.log("Missing number of occurrences");
+  } else {
+    const sum = parseInt(a) + parseInt(b);
+    console.log(sum);
+  }
 }
