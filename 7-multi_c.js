4f91b51ed8c4d12d5883fabfceda007774c2b934
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-19 18:26:45 +0000

    c multiplier

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+function printXTimesX(x) {
+     if (isNaN(x)){
+        console.log("Missing number of occurrences");
+     } else {
+        for (let i = 1; i <= x; i++) {
+            console.log("C is fun");
+        }
+     }
+}
+
+printXTimesX('ere')
