085519b7d3ddb8bf6a39f3093b6b14c07862e552
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-19 18:00:57 +0000

    added arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+// Determine the number of arguments and print the appropriate message
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
