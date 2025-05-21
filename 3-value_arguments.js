2103d11bf972c82600a66a34916f56739f459523
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-21 17:58:13 +0000

    added value_arguments

--- 3-value_arguments.js
+++ 3-value_arguments.js
@@ -1,2 +1,6 @@
-const args = process.argv[2];
-console.log(args||"No argument");
+const args = process.argv.slice(2);
+if (args[0] === undefined){
+    console.log("No argument");
+} else {
+    console.log(args[0]);
+}
