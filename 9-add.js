fdaf111d9a1a19d75fe97b4b6e94be12032de60f
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-21 10:16:02 +0000

    added NaN statement

--- 9-add.js
+++ 9-add.js
@@ -1,5 +1,14 @@
-function add(a, b) {
-  return a + b;
+
+
+function add(a,b){
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  } else {
+    return a + b;
+  }
 }
 
-console.log(add(2, 3));
+console.log(add())
+console.log(add(1))
+console.log(add(1,7))
+console.log(add(13,89))
