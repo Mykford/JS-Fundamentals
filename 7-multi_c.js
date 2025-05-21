28a05d925487462236eefff7ee7c985e965c22ae
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-21 19:07:12 +0000

    convert to integer

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,13 @@
-function printXTimesX(x) {
-     if (isNaN(x)){
-        console.log("Missing number of occurrences");
-     } else {
-        for (let i = 1; i <= x; i++) {
-            console.log("C is fun");
-        }
-     }
-}
 
-printXTimesX('ere')
+
+let args = process.argv.slice(2);
+
+if (!args[0]){
+   console.log("Missing number of occurrences");
+}else if(Number.isInteger(parseInt(args[0]))){
+    let i = 0;
+    while (i < parseInt(args[0])){
+        console.log("C is fun");
+        i++;
+    }
+}
